b8ade58be933eb257120afd0a7284071606425ba
Dustin Kahn <dustin@dustinkahn.com>
2016-12-09 11:41:24 -0500

    refactor of main.js with addition of unsubscribe

--- introduction-to-redux/src/main.js
+++ introduction-to-redux/src/main.js
@@ -1,17 +1,14 @@
 import 'babel-polyfill';
 import store from './stores/store';
 
+let unsubscribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
 console.log('Initial State of Store');
 console.log(store.getState());
 
 store.dispatch({ type: 'ADD_ONE' });
-console.log(store.getState());
-
 store.dispatch({ type: 'RESET_TO_ZERO' });
-console.log(store.getState());
-
 store.dispatch({ type: 'INCREASE_BY', numberToAdd: 2 });
-console.log(store.getState());
-
 store.dispatch({ type: 'SUBTRACT_BY', numberToSubtract: 2 });
-console.log(store.getState());
