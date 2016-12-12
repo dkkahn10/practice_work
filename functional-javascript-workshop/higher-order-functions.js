b3e3b64c11023cb39dfd979b465054c0713f4d34
Dustin Kahn <dustin@dustinkahn.com>
2016-12-12 16:21:17 -0500

    alternate solution added to challenge higher-order-functions

--- functional-javascript-workshop/higher-order-functions.js
+++ functional-javascript-workshop/higher-order-functions.js
@@ -5,3 +5,11 @@ function repeat(operation, num) {
 }
 
 module.exports = repeat;
+
+// function repeat(operation, num) {
+//  if (num <= 0) return
+//  operation()
+//  return repeat(operation, --num)
+// }
+
+// module.exports = repeat;
