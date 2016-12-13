9bc60e14e743d805669d9779a8da816ec5e6267d
Dustin Kahn <dustin@dustinkahn.com>
2016-12-13 12:07:38 -0500

    alternate solution added in comments

--- functional-javascript-workshop/reduce.js
+++ functional-javascript-workshop/reduce.js
@@ -12,3 +12,12 @@ function countWords(inputWords) {
 };
 
 module.exports = countWords;
+
+//function countWords(arr) {
+//  return arr.reduce(function(countMap, word) {
+//    countMap[word] = ++countMap[word] || 1;
+//    return countMap;
+//  }, {});
+//};
+
+//module.exports = countWords;
