3f75a25893ccc1035a18024a67b6316c29e4f5c1
Dustin Kahn <dustin@dustinkahn.com>
2016-12-22 21:38:15 -0500

    refactor of code to include divideFiveByNumber

--- es6-promises-and-fetch-api/src/main.js
+++ es6-promises-and-fetch-api/src/main.js
@@ -15,10 +15,10 @@ let getDataAsynchronously = ()  => {
 let divideFiveByNumber = (number, callback)  => {
   if (number !== 0) {
     let result =  5 / number;
-    callback(result, null);
+    return result;
   } else {
     let error = new Error('cannot divide by zero');
-    callback(null, error);
+    throw(error);
   }
 };
 
@@ -26,9 +26,7 @@ let reportResult = result => console.log('Result:', result);
 let reportError = error => console.error('Error:', error.message);
 
 let promiseOne = getDataAsynchronously();
-let promiseTwo = promiseOne.then(promiseOneValue => {
-  throw(new Error('error from onFulfilled callback'))
-});
-let promiseThree = promiseTwo.then(promiseTwoValue => console.log(promiseTwoValue));
-promiseThree.catch(promiseThreeValue => console.log(promiseThreeValue.message));
+let promiseTwo = promiseOne.then(divideFiveByNumber);
+let promiseThree = promiseTwo.then(reportResult);
+promiseThree.catch(reportError);
 console.log('Run before asynchronous function finishes executing');
