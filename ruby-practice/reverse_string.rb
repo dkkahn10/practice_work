83317bfc0e62a755736e1961cbf91614808cf71b
Dustin Kahn <dustin@dustinkahn.com>
2017-03-16 11:53:42 -0400

    reverse_string using extra array

--- ruby-practice/reverse_string.rb
+++ ruby-practice/reverse_string.rb
@@ -1,21 +1,32 @@
+# def reverse_string(str)
+#   array = str.split('')
+#
+#   x = 0
+#   y = array.length - 1
+#
+#   while x < y
+#     item1 = array[x]
+#     item2 = array[y]
+#
+#     array[x] = item2
+#     array[y] = item1
+#
+#     x += 1
+#     y -= 1
+#   end
+#
+#   array.join('')
+# end
+
 def reverse_string(str)
-  array = str.split('')
+  new_array = []
+  str_array = str.split('')
 
-  x = 0
-  y = array.length - 1
-
-  while x < y
-    item1 = array[x]
-    item2 = array[y]
-
-    array[x] = item2
-    array[y] = item1
-
-    x += 1
-    y -= 1
+  str_array.each do |letter|
+    new_array.unshift(letter)
   end
 
-  array.join('')
+  new_array.join('')
 end
 
 puts reverse_string("willow")
