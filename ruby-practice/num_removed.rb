cf2b42a5182f9e454edcdfd239f9aa442f94d84a
Dustin Kahn <dustin@dustinkahn.com>
2017-03-16 11:42:51 -0400

    number removed refactor to use subtraction of arrays

--- ruby-practice/num_removed.rb
+++ ruby-practice/num_removed.rb
@@ -1,16 +1,21 @@
+# def num_removed(list)
+#   this_sum = list.reduce(:+)
+#   num = 0
+#   sum = 0
+#
+#   while num <= 100
+#     sum += num
+#     num += 1
+#   end
+#
+#   sum - this_sum
+# end
+
 def num_removed(list)
-  this_sum = list.reduce(:+)
-  num = 0
-  sum = 0
-
-  while num <= 100
-    sum += num
-    num += 1
-  end
-
-  sum - this_sum
+  full_list = (1..100).to_a
+  missing_num = (full_list - list)[0]
 end
 
 array = (1..100).to_a
-array.delete(37)
+array.delete(47)
 puts num_removed(array)
